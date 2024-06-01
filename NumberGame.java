f0c56e8119d62610173aeffe5176ec586b71cf3b
Prajwal Bawanthare <150644082+prajwal2004-07@users.noreply.github.com>
2024-06-01 09:59:48 +0530

    Adding the Guessing Limit in the Game

--- NumberGame.java
+++ NumberGame.java
@@ -5,7 +5,7 @@ public class NumberGame {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Number Game");
+        System.out.println("Number Game ! You have only 6 Attempt to Guess Number");
 
         // Generating a random number using the random function.
         Random random = new Random();
@@ -15,6 +15,14 @@ public class NumberGame {
         int userInput = sc.nextInt();
 
         while (userInput >= 0 && userInput < 100) {
+
+            if (guessingChances > 6) {
+
+                System.out.println("You have used all your chances to guess the Random Number");
+                System.out.println("The Random Number was: " + randomNumber);
+                System.out.println("You have used " + guessingChances + " chances to guess the Random Number");
+                break;
+            }
             if (userInput <= 0) {
                 System.out.println("Please Enter a Number Greater than or equal to 0");
                 System.out.print("Enter the Number: ");
@@ -31,7 +39,7 @@ public class NumberGame {
                 System.out.println("Your Number is Lower than the Random Number");
                 System.out.print("Enter the Number: ");
                 userInput = sc.nextInt();
-            } else {
+            } else{
                 System.out.println("Congratulations! You Guessed the Random Number: " + randomNumber);
                 System.out.println("The Random Number was: " + randomNumber);
                 System.out.println("You have used " + guessingChances + " chances to guess the Random Number");
