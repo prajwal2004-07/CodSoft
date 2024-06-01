b8921aaaf3f857004a25bd8b6010b32a25ea1525
Prajwal Bawanthare <150644082+prajwal2004-07@users.noreply.github.com>
2024-06-01 09:38:19 +0530

    Project 1: Number Guessing Game

---
+++ NumberGame.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class NumberGame {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Number Game");
+
+        // Generating a random number using the random function.
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        int guessingChances = 0;
+        System.out.print("Enter the Number for Guessing the Random Number: ");
+        int userInput = sc.nextInt();
+
+        while (userInput >= 0 && userInput < 100) {
+            if (userInput <= 0) {
+                System.out.println("Please Enter a Number Greater than or equal to 0");
+                System.out.print("Enter the Number: ");
+                userInput = sc.nextInt();
+            } else if (userInput >= 100) {
+                System.out.println("Please Enter a Number Lower than 100");
+                System.out.print("Enter the Number: ");
+                userInput = sc.nextInt();
+            } else if (userInput > randomNumber) {
+                System.out.println("Your Number is Higher than the Random Number");
+                System.out.print("Enter the Number: ");
+                userInput = sc.nextInt();
+            } else if (userInput < randomNumber) {
+                System.out.println("Your Number is Lower than the Random Number");
+                System.out.print("Enter the Number: ");
+                userInput = sc.nextInt();
+            } else {
+                System.out.println("Congratulations! You Guessed the Random Number: " + randomNumber);
+                System.out.println("The Random Number was: " + randomNumber);
+                System.out.println("You have used " + guessingChances + " chances to guess the Random Number");
+                break;
+            }
+            guessingChances++;
+        }
+        // Close the Scanner object
+        sc.close();
+    }
+}
